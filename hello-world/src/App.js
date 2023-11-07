a18ea3a0bec51d5b98ea1013fdddcee8d7f74a29
Bui The Hoa <buithehoa@gmail.com>
2023-11-07 12:50:50 +0700

    Add userName.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const userName = 'HOFA'
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, {userName}
         </a>
       </header>
     </div>
