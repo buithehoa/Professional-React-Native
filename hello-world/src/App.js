c1f84e87d7894cfe0e3d8936403ec8668bd7b9e9
Bui The Hoa <buithehoa@gmail.com>
2023-12-26 17:08:54 +0700

    Remove ReactButton.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -11,23 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <ReactButton/>
       </header>
     </div>
   );
-}
-
-function ReactButton(userName) {
-  const link = 'https://reactjs.org';
-  return (
-    <a
-      className="App-link"
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer">
-      Learn React, {userName}
-    </a>
-  )
 }
 
 export default App;
