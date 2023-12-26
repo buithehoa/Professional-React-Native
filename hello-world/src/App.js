76208aacd05ebae61d0a2b82271ac27e6e13f510
Bui The Hoa <buithehoa@gmail.com>
2023-12-26 16:50:47 +0700

    Add workspace file.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -11,17 +11,23 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, {userName}
-        </a>
+        <ReactButton/>
       </header>
     </div>
   );
+}
+
+function ReactButton(userName) {
+  const link = 'https://reactjs.org';
+  return (
+    <a
+      className="App-link"
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer">
+      Learn React, {userName}
+    </a>
+  )
 }
 
 export default App;
